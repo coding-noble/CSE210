9573905664c42447f0f530b0df84bc93f359fcf3
coding-noble <milosdragons@gmail.com>
2023-02-21 11:05:39 -0700

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string playAgain = "yes";
+        int userGuess = -1;
+        int magicNumber;
+        int guessCount;
+
+        while (playAgain.ToLower() == "yes")
+        {
+            guessCount = 0;
+            magicNumber = randomGenerator.Next(1, 101);
+            Console.WriteLine(magicNumber);
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your userGuess? ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > userGuess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < userGuess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You userGuessed it!");
+                }
+
+                guessCount += 1;
+            }
+            Console.WriteLine($"It took you {guessCount} try(s).");
+            Console.Write($"Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
