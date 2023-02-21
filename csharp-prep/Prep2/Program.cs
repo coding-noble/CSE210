9856cd1015cebcd38a0ed97d327b19d53be3eb3b
coding-noble <milosdragons@gmail.com>
2023-02-21 10:51:22 -0700

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool passed = false;
+        char letter = 'F';
+        char modifier = ' ';
+        Console.Write("What is your grade percentage? ");
+        string userGradePer = Console.ReadLine();
+
+        if (int.Parse(userGradePer) >= 90)
+        {
+            letter = 'A';
+            passed = true;
+        }
+        else if (int.Parse(userGradePer) >= 80)
+        {
+            letter = 'B';
+            passed = true;
+        }
+        else if (int.Parse(userGradePer) >= 70)
+        {
+            letter = 'C';
+            passed = true;
+        }
+        else if (int.Parse(userGradePer) >= 60)
+        {
+            letter = 'D';
+        }
+
+        if (letter != 'F' && (int.Parse(userGradePer) % 10) >= 7)
+        {
+            modifier = '+';
+        }
+        else if (letter != 'F' && (int.Parse(userGradePer) % 10) <= 3)
+        {
+            modifier = '-';
+        }
+        if (letter == 'A' && modifier == '+')
+        {
+            modifier = ' ';
+        }
+
+        Console.WriteLine($"You got a {letter}{modifier}");
+
+        if (passed)
+        {
+            Console.WriteLine("You Passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time :(");
+        }
     }
 }
