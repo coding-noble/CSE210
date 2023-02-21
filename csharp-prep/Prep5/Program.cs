16de5e6d409124046ba58f35eb27d81dc4d9a2a2
coding-noble <milosdragons@gmail.com>
2023-02-21 11:50:54 -0700

    Learning Activity - C# Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    private static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+    }
+
+    private static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    private static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    private static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    private static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine();
+        Console.WriteLine();
     }
 }
