9cec60ed4abf5faa5c40a9c12da4af478ab1e92c
coding-noble <milosdragons@gmail.com>
2023-03-07 13:03:26 -0700

    W04 Prepare: Learning Activity

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,17 @@
+public class Assignment
+{
+    protected string _studentName, _topic;
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
